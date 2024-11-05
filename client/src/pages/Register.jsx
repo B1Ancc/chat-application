2b669d5319f5c09d917261616f5b2e4c3c93faaf
REXV <bagaming40@gmail.com>
2024-11-06 02:23:12 +0700

    modified:   client/src/context/AuthContext.jsx 	modified:   client/src/pages/Register.jsx 	new file:   client/src/utils/Services.jsx

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -3,11 +3,12 @@ import { Alert, Button, Form, Row, Col, Stack } from "react-bootstrap";
 import { AuthContext } from "../context/AuthContext";
 
 const Register = () => {
-  const { registerInfo, updateRegisterInfo } = useContext(AuthContext);
+  const { registerInfo, updateRegisterInfo, registerUser, registerError, isRegisterLoading } =
+    useContext(AuthContext);
 
   return (
     <>
-      <Form>
+      <Form onSubmit={registerUser}>
         <Row
           style={{
             height: "100vh",
@@ -36,15 +37,20 @@ const Register = () => {
                 type="password"
                 placeholder="Mật khẩu"
                 onChange={(e) =>
-                  updateRegisterInfo({ ...registerInfo, password: e.target.value })
+                  updateRegisterInfo({
+                    ...registerInfo,
+                    password: e.target.value,
+                  })
                 }
               />
               <Button variant="primary" type="submit">
-                Đăng ký
+                {isRegisterLoading ? "Đang tạo tài khoản cho bạn" : "Đăng ký"}
               </Button>
-              <Alert variant="danger">
-                <p>Đã có lỗi xảy ra.</p>
-              </Alert>
+              {registerError?.error && (
+                <Alert variant="danger">
+                  <p>{registerError?.message}</p>
+                </Alert>
+              )}
             </Stack>
           </Col>
         </Row>

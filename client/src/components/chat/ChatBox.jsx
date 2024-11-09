33b437f78b320ad09abc4c04b2912cf0656e1c3f
REXV <bagaming40@gmail.com>
2024-11-09 17:10:34 +0700

    feat: part 15 done

--- client/src/components/chat/ChatBox.jsx
+++ client/src/components/chat/ChatBox.jsx
@@ -1,14 +1,22 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "../../context/AuthContext";
 import { ChatContext } from "../../context/ChatContext";
 import { useFetchRecipientUser } from "../../hooks/useFetchRecipient";
 import { Stack } from "react-bootstrap";
+import InputEmoji from "react-input-emoji";
+import sendIcon from "../../assets/send-fill.svg";
 import moment from "moment";
+import "moment/dist/locale/vi";
+moment.locale("vi");
 
 const ChatBox = () => {
   const { user } = useContext(AuthContext);
-  const { currentChat, messages, isMessagesLoading } = useContext(ChatContext);
+  const { currentChat, messages, isMessagesLoading, sendTextMessage } =
+    useContext(ChatContext);
   const { recipientUser } = useFetchRecipientUser(currentChat, user);
+  const [textMessage, setTextMessage] = useState("");
+
+  console.log("text", textMessage);
 
   if (!recipientUser)
     return (
@@ -32,11 +40,38 @@ const ChatBox = () => {
       <Stack gap={3} className="messages">
         {messages &&
           messages.map((message, index) => (
-            <Stack key={index} className={`${message?.senderId === user?._id ? "message self align-self-end flex-grow-0" : "message align-self-start flex-grow-0"}`}>
+            <Stack
+              key={index}
+              className={`${
+                message?.senderId === user?._id
+                  ? "message self align-self-end flex-grow-0"
+                  : "message align-self-start flex-grow-0"
+              }`}
+            >
               <span>{message.text}</span>
-              <span className="message-footer">{moment(message.createdAt).calendar()}</span>
+              <span className="message-footer">
+                {moment(message.createdAt).calendar()}
+              </span>
             </Stack>
           ))}
+      </Stack>
+      <Stack direction="horizontal" gap={3} className="chat-input flex-grow-0">
+        <InputEmoji
+          value={textMessage}
+          onChange={setTextMessage}
+          fontFamily="Montserrat"
+          borderColor="rgba(72, 112, 22, 0.2)"
+          language="vi"
+          placeholder="Nhắn tin"
+        />
+        <button
+          className="send-btn"
+          onClick={() =>
+            sendTextMessage(textMessage, user, currentChat._id, setTextMessage)
+          }
+        >
+          <img src={sendIcon} height="16px" background-color="white" />
+        </button>
       </Stack>
     </Stack>
   );
--- client/src/context/ChatContext.jsx
+++ client/src/context/ChatContext.jsx
@@ -12,6 +12,8 @@ export const ChatContextProvider = ({ children, user }) => {
   const [messages, setMessages] = useState(null);
   const [isMessagesLoading, setIsMessagesLoading] = useState(false);
   const [messagesError, setMessagesError] = useState(null);
+  const [sendTextMessageError, setSendTextMessageError] = useState(null);
+  const [newMessage, setNewMessage] = useState(null);
 
   console.log("messages", messages);
 
@@ -86,6 +88,30 @@ export const ChatContextProvider = ({ children, user }) => {
     getMessages();
   }, [currentChat]);
 
+  const sendTextMessage = useCallback(
+    async (textMessage, sender, currentChatId, setTextMessage) => {
+      if (!textMessage) return console.log("Hãy nhập gì đó.");
+
+      const response = await postRequest(
+        `${baseUrl}/tin-nhan`,
+        JSON.stringify({
+          chatId: currentChatId,
+          senderId: sender._id,
+          text: textMessage,
+        })
+      );
+
+      if (response.error) {
+        return setSendTextMessageError(response);
+      }
+
+      setNewMessage(response);
+      setMessages((prev) => [...prev, response]);
+      setTextMessage("");
+    },
+    []
+  );
+
   const updateCurrentChat = useCallback((chat) => {
     setCurrentChat(chat);
   }, []);
@@ -119,6 +145,7 @@ export const ChatContextProvider = ({ children, user }) => {
         messages,
         isMessagesLoading,
         messagesError,
+        sendTextMessage,
       }}
     >
       {children}
